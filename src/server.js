3d37f9d9ff6a26b631ea93e1a8ec33164ce4603b
Ismael <yasuobestgm@gmail.com>
2021-01-24 05:00:15 -0300

    att

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@ const { exec } = require("child_process")
 const express = require ("express")
 const server = express ()
 
+server.use(express.static("public"))
+
 server.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
+   
 })
 
 
