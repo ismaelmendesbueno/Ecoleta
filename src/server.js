a447b86fe63e4c2e53587ceb496e9acad8f8cd21
Ismael <yasuobestgm@gmail.com>
2021-01-24 07:01:10 -0300

    final da aula4

--- src/server.js
+++ src/server.js
@@ -1,13 +1,36 @@
 const { exec } = require("child_process")
 const express = require ("express")
+
+
 const server = express ()
 
 server.use(express.static("public"))
 
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true 
+    
+})
+
+
+
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
    
 })
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+   
+})
+
+server.get("/search", (req, res) => {
+    return res.render( "search-results.html")
+   
+})
+
+
 
 
 
