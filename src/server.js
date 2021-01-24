3bc69434ff7381edcee1ef2cb8a9225dcc8db79b
Ismael <yasuobestgm@gmail.com>
2021-01-24 04:41:38 -0300

    Modificaçao das pastas

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const { exec } = require("child_process")
+const express = require ("express")
+const server = express ()
+
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+
+
+
+server.listen (3000)
